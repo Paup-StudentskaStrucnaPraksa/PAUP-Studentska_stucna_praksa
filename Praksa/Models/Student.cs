46dfb6d9e466a6a8ca6616e4d041e68cd6d5c5fc
MajaHorvatic <majek.horvatic@gmail.com>
2017-06-03 21:10:11 +0200

    Dodani Display Atributi
    Dodani Display Atributi u klasu Student i Poduzece

--- Praksa/Models/Student.cs
+++ Praksa/Models/Student.cs
@@ -11,15 +11,25 @@ namespace Praksa.Models
     public class Student
     {
         [Key]
+        [Display (Name ="Matični broj")]
         public string maticniBroj { get; set; }
+        [Display(Name = "Ime")]
         public string ime { get; set; }
+        [Display(Name = "Prezime")]
         public string prezime { get; set; }
+        [Display(Name = "Adresa stanovanja")]
         public string adresaStanovanja { get; set; }
+        [Display(Name = "E-mail")]
         public string mail { get; set; }
+        [Display(Name = "Telefon")]
         public string telefon { get; set; }
+        [Display(Name = "Smjer studija")]
         public string smjerStudija { get; set; }
+        [Display(Name = "Godina studija")]
         public int godinaStudija { get; set; }
+        [Display(Name = "Korisničko ime")]
         public string korisnickoIme { get; set; }
+        [Display(Name = "Lozinka")]
         public string lozinka { get; set; }
 
     }
