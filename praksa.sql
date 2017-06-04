c6505665369269de42de0f77d316a7bde0b839f5
mihoci <mihoci.patrik166@gmail.com>
2017-06-04 20:21:50 +0200

    updejtana baza

--- praksa.sql
+++ praksa.sql
@@ -45,7 +45,7 @@ CREATE TABLE `poduzeca` (
 
 LOCK TABLES `poduzeca` WRITE;
 /*!40000 ALTER TABLE `poduzeca` DISABLE KEYS */;
-INSERT INTO `poduzeca` VALUES (1,'Prvo','M.Gupca 20','prvo@prvo.hr','prvo.hr','0971234567','040825456','Štef','vrlo važno poduzece',0,0),(2,'Drugo','Trg maršala tita','drugo@drugo.hr','drugo.hr','0991234567','042258789','Mirko','još važnije poduzece',1,0);
+INSERT INTO `poduzeca` VALUES (1,'Prvo','M.Gupca 20','prvo@prvo.hr','prvo.hr','0971234567','040825456','Štef','vrlo važno poduzece',1,0),(2,'Drugo','Trg maršala tita','drugo@drugo.hr','drugo.hr','0991234567','042258789','Mirko','još važnije poduzece',1,0);
 /*!40000 ALTER TABLE `poduzeca` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,8 +62,9 @@ CREATE TABLE `praksa` (
   `id_poduzeca` int(11) NOT NULL,
   `zavrseno` tinyint(4) DEFAULT '0',
   `zakljucano` tinyint(4) DEFAULT '0',
+  `godina` year(4) NOT NULL,
   PRIMARY KEY (`idpraksa`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +73,7 @@ CREATE TABLE `praksa` (
 
 LOCK TABLES `praksa` WRITE;
 /*!40000 ALTER TABLE `praksa` DISABLE KEYS */;
+INSERT INTO `praksa` VALUES (1,'1234567890123',1,0,0,0000),(2,'1234567890123',1,0,0,2017),(3,'1234567890123',2,0,0,2017),(4,'1232343454567',2,0,0,2017);
 /*!40000 ALTER TABLE `praksa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +95,9 @@ CREATE TABLE `student` (
   `godinaStudija` int(1) NOT NULL,
   `korisnickoIme` varchar(20) NOT NULL,
   `lozinka` varchar(20) NOT NULL,
-  PRIMARY KEY (`maticniBroj`)
+  PRIMARY KEY (`maticniBroj`),
+  UNIQUE KEY `mail_UNIQUE` (`mail`),
+  UNIQUE KEY `maticniBroj_UNIQUE` (`maticniBroj`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -103,7 +107,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('0','admin','admin','admin','admin@admin.com','admin','admin',0,'admin','!2Asdf'),('1234567890123','Ana','Anic','anina adresa','ana@anic.com','123456','Racunalstvo',2,'ana','!2Asdf');
+INSERT INTO `student` VALUES ('1232343454567','Marko','Markic','Markograd','marko@marko.com','098172','Održivi razvoj',1,'markec','!2Asdf'),('1234567890123','Ana','Anic','anina adresa','ana@anic.com','123456','Racunalstvo',2,'ana','!2Asdf');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +120,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-04 17:04:51
+-- Dump completed on 2017-06-04 20:21:16
